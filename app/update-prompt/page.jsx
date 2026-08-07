1be8953f22899118222dfce2c243f48a03ad18d4
Kristofer <kristhajeb@gmail.com>
2026-08-07 15:09:56 +0800

    second commit

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -5,7 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@/components/Form";
 
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
@@ -65,4 +65,4 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
